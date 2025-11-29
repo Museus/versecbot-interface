b0b95f7456157277e0dc33a4e27c4911c17f8999
Museus <museus@protonmail.com>
2025-11-28 23:43:02 -0500

    v0.4.1: remove unused logging module

--- src/versecbot_interface/logging.py
+++
@@ -1,8 +0,0 @@
-from logging import getLogger, Logger
-
-
-def get_plugin_logger(name: str) -> Logger:
-    """
-    Returns a logger for the specified plugin name.
-    """
-    return getLogger(f"versecbot.plugins.{name}")
